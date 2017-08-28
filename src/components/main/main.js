e56ecfc310e3ef98800bde82e7f7111f9fb583ce
XiaoFangL <ex_lvxiaofang@finruntech.com>
2017-08-28 15:42:18 +0800

    echarts

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { setHistory } from '../../redux/action'
+import { setHistory } from '../../redux/action';
+import echarts from 'echarts';
 import '../../style/main.css';
 
 class Main extends Component {
     componentDidMount(){
-        this.props.setHistory(this.props.history)
+        this.props.setHistory(this.props.history);
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('main'));
+        // 绘制图表
+        myChart.setOption({
+            title: { text: 'ECharts 入门示例' },
+            tooltip: {},
+            xAxis: {
+                data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        });
     }
 
     render() {
         return (
             <div>
-                <div>主页默认页面</div>
+                <div id="main" style={{ width: 1000, height: 800 }}></div>
             </div>
         )
     }
